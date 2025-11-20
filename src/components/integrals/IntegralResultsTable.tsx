86ccec1bb7b17b76a08646412ecfb4d45146c5a7
Anastasiia <anastasia.tvf@gmail.com>
2025-11-21 00:25:44 +0200

    fix styles

--- src/components/integrals/IntegralResultsTable.tsx
+++ src/components/integrals/IntegralResultsTable.tsx
@@ -15,7 +15,6 @@ export function IntegralResultsTable({ results }: IntegralResultsTableProps) {
                     width: "100%",
                     borderCollapse: "collapse",
                     fontSize: "14px",
-                    color: "#111827",
                 }}
             >
                 <thead>
